3d92997c113060d6a25ca072376d14496f5bd057
zhenac <zhen.a.chen@nokia-sbell.com>
2020-08-11 09:49:30 +0800

    Add file

---
+++ web/webflux/src/main/java/tk/lens/solution/web/webflux/WebfluxApplication.java
@@ -0,0 +1,18 @@
+package tk.lens.solution.web.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main Boot
+ *
+ */
+@SpringBootApplication
+public class WebfluxApplication 
+{
+	
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(WebfluxApplication.class, args);
+    }
+}
