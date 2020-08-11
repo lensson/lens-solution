65b4cda63b77a25e921d3a395dd01476cae7b8cc
zhenac <zhen.a.chen@nokia-sbell.com>
2020-08-11 12:41:04 +0800

    commit 4

---
+++ web/webflux/src/main/java/tk/lens/solution/web/webflux/web/HelloController.java
@@ -0,0 +1,17 @@
+package tk.lens.solution.web.webflux.web;
+
+import java.util.Locale;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+    @RequestMapping("/hello")
+	public String hello(Locale locale, Model model) {
+		return "Hello World";
+	}
+
+}
