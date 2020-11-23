a32605bb88cecebb93cc5977bcec5947e22bfab1
zhenac <zhen.a.chen@nokia-sbell.com>
2020-11-23 17:09:05 +0800

    init

--- lens-blog/lens-blog-admin-vue-frontend/config/prod.env.js
+++ lens-blog/lens-blog-admin-vue-frontend/config/prod.env.js
@@ -3,19 +3,19 @@ module.exports = {
   NODE_ENV: '"production"',
 
   //生产环境
-  ADMIN_API: '"http://47.115.54.30:8601"',
-  PICTURE_API: '"http://47.115.54.30:8602"',
-  WEB_API: '"http://47.115.54.30:8603"',
-  Search_API: '"http://47.115.54.30:8605"',
-  SPRING_BOOT_ADMIN: '"http://47.115.54.30:8606/wallboard"',
-  SOLR_API: '"http://47.115.54.30:8080/solr"',
-  Zipkin_Admin: '"http://47.115.54.30:9411/zipkin/"',
-  ELASTIC_SEARCH: '"http://47.115.54.30:5601"',
-  EUREKA_API: '"http://47.115.54.30:8761"',
-  RABBIT_MQ_ADMIN: '"http://47.115.54.30:15672"',
-  DRUID_ADMIN: '"http://47.115.54.30:8601/druid/login.html"',
+  ADMIN_API: '"http://106.75.6.129:9002"',
+  PICTURE_API: '"http://106.75.6.129:9012"',
+  WEB_API: '"http://106.75.6.129:9001"',
+  Search_API: '"http://106.75.6.129:9013"',
+  SPRING_BOOT_ADMIN: '"http://106.75.6.129:9020/wallboard"',
+  SOLR_API: '"http://106.75.6.129:8983/solr"',
+  Zipkin_Admin: '"http://106.75.6.129:9411/zipkin/"',
+  ELASTIC_SEARCH: '"http://106.75.6.129:5601"',
+  EUREKA_API: '"http://106.75.6.129:8761"',
+  RABBIT_MQ_ADMIN: '"http://106.75.6.129:15672"',
+  DRUID_ADMIN: '"http://106.75.6.129:8601/druid/login.html"',
   // 有域名
   BLOG_WEB_URL: '"http://demoweb.moguit.cn"',
   // 没有域名
-  // BLOG_WEB_URL: '"http://47.115.54.30:9527"',
+  // BLOG_WEB_URL: '"http://106.75.6.129:9527"',
 }
