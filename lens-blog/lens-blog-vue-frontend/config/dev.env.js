f285e08466ac15999d23423016df87ac9569348f
zhenac <zhen.a.chen@nokia-sbell.com>
2020-11-30 16:09:28 +0800

    init

--- lens-blog/lens-blog-vue-frontend/config/dev.env.js
+++ lens-blog/lens-blog-vue-frontend/config/dev.env.js
@@ -5,9 +5,9 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
 
-  VUE_MOGU_WEB: '"http://172.17.0.1:8001"',
-  PICTURE_API: '"http://172.17.0.1:9012"',
-	WEB_API: '"http://172.17.0.1:9001"',
-	ELASTICSEARCH: '"http://172.17.0.1:9200"',
-  
+  VUE_MOGU_WEB: '"http://172.28.0.1:8001"',
+  PICTURE_API: '"http://172.28.0.1:9012"',
+	WEB_API: '"http://172.28.0.1:9001"',
+	ELASTICSEARCH: '"http://172.28.0.1:9200"',
+
 })
--- lens-blog/lens-blog-vue-frontend/config/prod.env.js
+++ lens-blog/lens-blog-vue-frontend/config/prod.env.js
@@ -4,10 +4,10 @@ module.exports = {
   //配置线上环境
 
   // 如果没有域名使用这个
-  //VUE_MOGU_WEB: '"http://172.17.0.1:8602"',
+  //VUE_MOGU_WEB: '"http://172.28.0.1:8602"',
   // 如果有域名使用这个
   VUE_MOGU_WEB: '"http://www.malakj.com"',
-  PICTURE_API: '"http://172.17.0.1:9012"',
-  WEB_API: '"http://172.17.0.1:9001"',
-  ELASTICSEARCH: '"http://172.17.0.1:9200"',
+  PICTURE_API: '"http://172.28.0.1:9012"',
+  WEB_API: '"http://172.28.0.1:9001"',
+  ELASTICSEARCH: '"http://172.28.0.1:9200"',
 }
