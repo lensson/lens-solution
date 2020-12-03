fded0a13011db47456c0043cad24d7ec02531d56
zhenac <zhen.a.chen@nokia-sbell.com>
2020-12-03 17:17:20 +0800

    init

---
+++ lens-blog/lens-blog-admin-frontend/vue.config.js
@@ -0,0 +1,14 @@
+// const path = require('path')
+//
+// function resolve(dir) {
+//   return path.join(__dirname, dir)
+// }
+
+const vueConfig = {
+  devServer: {
+    port: 8002
+  },
+  lintOnSave: true
+}
+
+module.exports = vueConfig
