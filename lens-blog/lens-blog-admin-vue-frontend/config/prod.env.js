4820cc87fb063a25c167b0d6a5e12c1dda2aa3fe
zhenac <zhen.a.chen@nokia-sbell.com>
2021-10-13 14:56:05 +0800

    init

--- lens-blog/lens-blog-admin-vue-frontend/config/prod.env.js
+++ lens-blog/lens-blog-admin-vue-frontend/config/prod.env.js
@@ -4,18 +4,18 @@ module.exports = {
 
   //生产环境
   ADMIN_API: '"/api/v1/blog/admin"',
-  PICTURE_API: '"https://www.malakj.com/api/v1/blog/picture"',
-  WEB_API: '"https://www.malakj.com/api/v1/blog/web"',
-  Search_API: '"https://www.malakj.com/api/v1/blog/search"',
-  SPRING_BOOT_ADMIN: '"https://www.malakj.com/wallboard"',
-  SOLR_API: '"https://solr.malakj.com"',
-  Zipkin_Admin: '"https://zipkin.malakj.com"',
-  ELASTIC_SEARCH: '"https://es.malakj.com"',
-  EUREKA_API: '"https://eureka.malakj.com"',
-  RABBIT_MQ_ADMIN: '"https://rabbit.malakj.com"',
+  PICTURE_API: '"https://blog.malakj.com/api/v1/blog/picture"',
+  WEB_API: '"https://blog.malakj.com/api/v1/blog/web"',
+  Search_API: '"https://blog.malakj.com/api/v1/blog/search"',
+  SPRING_BOOT_ADMIN: '"https://blog.malakj.com/wallboard"',
+  SOLR_API: '"https://malakj.com/solr"',
+  Zipkin_Admin: '"https://malakj.com/zipkin"',
+  ELASTIC_SEARCH: '"https://malakj.com/es"',
+  EUREKA_API: '"https://malakj.com/eureka"',
+  RABBIT_MQ_ADMIN: '"https://malakj.com/rabbit"',
   DRUID_ADMIN: '"https://druid.malakj.com/druid/login.html"',
   // 有域名
-  BLOG_WEB_URL: '"http://www.malakj.com"',
+  BLOG_WEB_URL: '"http://blog.malakj.com"',
   // 没有域名
   // BLOG_WEB_URL: '"http://106.75.6.129:9527"',
 }
