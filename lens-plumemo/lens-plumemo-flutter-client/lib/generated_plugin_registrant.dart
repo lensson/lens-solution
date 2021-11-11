2bfb3a23516e5f577752e7675e25ab35c595a599
zhenac <zhen.a.chen@nokia-sbell.com>
2021-11-11 08:44:53 +0800

    change lens-plumemo-flutter-client

--- lens-plumemo/lens-plumemo-flutter-client/lib/generated_plugin_registrant.dart
+++ lens-plumemo/lens-plumemo-flutter-client/lib/generated_plugin_registrant.dart
@@ -2,8 +2,8 @@
 // Generated file. Do not edit.
 //
 
-// ignore: unused_import
-import 'dart:ui';
+// ignore_for_file: directives_ordering
+// ignore_for_file: lines_longer_than_80_chars
 
 import 'package:fluttertoast/fluttertoast_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
@@ -12,9 +12,9 @@ import 'package:video_player_web/video_player_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
-void registerPlugins(PluginRegistry registry) {
-  FluttertoastWebPlugin.registerWith(registry.registrarFor(FluttertoastWebPlugin));
-  UrlLauncherPlugin.registerWith(registry.registrarFor(UrlLauncherPlugin));
-  VideoPlayerPlugin.registerWith(registry.registrarFor(VideoPlayerPlugin));
-  registry.registerMessageHandler();
+void registerPlugins(Registrar registrar) {
+  FluttertoastWebPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  VideoPlayerPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
 }
