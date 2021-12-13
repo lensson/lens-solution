6b5172559a7c50228848246e0beba40623735511
zhenac <zhen.a.chen@nokia-sbell.com>
2021-12-13 17:21:53 +0800

    update uni matadata

--- lens-blog/lens-blog-uniapp-frontend/config/config.js
+++ lens-blog/lens-blog-uniapp-frontend/config/config.js
@@ -3,8 +3,8 @@ export const appConfig = {
     tokenKey: 'Authorization',
 	
 	// 开发环境
-    WEB_API: 'http://localhost:8607/mogu-web',
-	PICTURE_API: 'http://localhost:8607/mogu-picture',
+    // WEB_API: 'http://localhost:8607/mogu-web',
+	// PICTURE_API: 'http://localhost:8607/mogu-picture',
 	
 	// 演示环境
 	// WEB_API: 'http://120.78.126.96:8607/mogu-web',
@@ -13,4 +13,7 @@ export const appConfig = {
 	// 正式环境
 	// WEB_API: 'https://apiweb.moguit.cn/mogu-web',
 	// PICTURE_API: 'https://apiweb.moguit.cn/mogu-picture',
+
+    WEB_API: 'https://blog.malakj.com/api/v1/blog/web',
+    PICTURE_API: 'https://blog.malakj.com/api/v1/blog/picture',
 }
