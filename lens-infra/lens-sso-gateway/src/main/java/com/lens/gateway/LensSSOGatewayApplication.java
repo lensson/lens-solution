2825603405c5fe5f8e2047fc620c6aa6f962b5c9
zhenac <zhen.a.chen@nokia-sbell.com>
2025-05-15 07:08:33 +0530

    add sso projects

---
+++ lens-infra/lens-sso-gateway/src/main/java/com/lens/gateway/LensSSOGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.lens.gateway;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LensSSOGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LensSSOGatewayApplication.class, args);
+    }
+}
